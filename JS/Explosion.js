46ca33bfaa9677c3e1b62fff7d3dc2613f3703ff
Kaneby <fabiankaneby@icloud.com>
2017-02-01 09:31:22 +0100

    Boost och Kill fixat

--- JS/Explosion.js
+++ JS/Explosion.js
@@ -32,15 +32,15 @@ function Explosion(startX, startY, whichPlayer, teamColour, explosionRange) {
         //mitten sprängning
         ctx.fillRect(this.xPos, this.yPos, 50, 50);
 
-        for (var i = 0; i <= this.rightExp; i++) { 
+        for (var i = 0; i <= this.rightExp; i++) {
             //höger sprängning
             if ((this.yPos / 50) % 2 == 0) {
-                
+
                 //console.log(i);
                 ctx.fillRect(this.xPos + 50 * i, this.yPos, 50, 50);
             }
         }
-        for (var i = 0; i <= this.rightExp; i++) {
+        for (var i = 0; i <= this.leftExp; i++) {
             //vänster sprängning
             if ((this.yPos / 50) % 2 == 0) {
                 ctx.fillRect(this.xPos - i * 50, this.yPos, 50, 50);
@@ -49,12 +49,12 @@ function Explosion(startX, startY, whichPlayer, teamColour, explosionRange) {
         for (var i = 0; i <= this.downExp; i++) {
             //ner sprängning
             if ((this.xPos / 50) % 2 == 0) {
-                ctx.fillRect(this.xPos, this.yPos + 50 * i, 50, 50);
+                ctx.fillRect(this.xPos, this.yPos + i * 50, 50, 50);
             }
         }
         for (var i = 0; i <= this.upExp; i++) {
             //upp sprängning
-            console.log(i, this.upExp);
+            //console.log(i, this.upExp);
             if ((this.xPos / 50) % 2 == 0) {
                 ctx.fillRect(this.xPos, this.yPos - i * 50, 50, 50);
             }
@@ -68,105 +68,151 @@ function Explosion(startX, startY, whichPlayer, teamColour, explosionRange) {
         //loopa igenom och kolla efter collision mella explosion och boxes, ta bort ev boxes
 
         for (var j = 0; j <= player.range; j++) {
-            
-            for (var i = 0; i < boxes.length; i++) {
-                
-                //console.log(j);
-                if (this.xPos + (j * 50) == boxes[i].xPos && this.yPos == boxes[i].yPos) {
 
-                    console.log("kolla höger");
+            for (var i = 0; i < boxes.length; i++) {
+
+                if (this.xPos + j * 50 == boxes[i].xPos && this.yPos == boxes[i].yPos) {
+
+
+                    console.log(j);
+
                     //ge chans till att släppa boost
                     if (Math.random() > 0.7) {
+
                         console.log("boost skapad");
                         boosters.push(new Boost(boxes[i].xPos, boxes[i].yPos));
                     }
-                    boxes.splice(i, 1);
-                    this.rightExp = j;
 
+                    //ta bort box och stäng av loop samt ge rightExp nytt värde för hur långt den ska spränga
+                    boxes.splice(i, 1);
+                    console.log("kolla vänster");
+                    this.rightExp = j;
+                    j = player.range + 1;
                     i = boxes.length + 1;
 
-                    //this.rightExp = false;
+                    //stäng av loop om den exploderar i sten
+                } else if ((this.yPos / 50) % 2 !== 0) {
+                    console.log("xpos/50");
+                    this.rightExp = 1;
+                    j = player.range + 1;
+                    i = boxes.length + 1;
 
-                } else {
-                    
+                    //om den går igenom hela loopen plyer rightexp = player.range
+                } else if (i == boxes.length - 1) {
+                    console.log("sista else this up = player range vänster");
                     this.rightExp = player.range;
                 }
-
             }
         }
         for (var j = 0; j <= player.range; j++) {
-            //console.log(j);
+
             for (var i = 0; i < boxes.length; i++) {
 
                 if (this.xPos - j * 50 == boxes[i].xPos && this.yPos == boxes[i].yPos) {
 
-                    console.log("kolla vänster");
-                    //ge chans till att  släppa boost
+
+                    console.log(j);
+
+                    //ge chans till att släppa boost
                     if (Math.random() > 0.7) {
+
                         console.log("boost skapad");
                         boosters.push(new Boost(boxes[i].xPos, boxes[i].yPos));
                     }
-                    boxes.splice(i, 1);
-                    this.leftExp = j;
 
+                    //ta bort box och stäng av loop samt ge leftexp nytt värde för hur långt den ska spränga
+                    boxes.splice(i, 1);
+                    console.log("kolla vänster");
+                    this.leftExp = j;
+                    j = player.range + 1;
                     i = boxes.length + 1;
-                } else {
-                    
+
+                    //stäng av loop om den exploderar i sten
+                } else if ((this.yPos / 50) % 2 !== 0) {
+                    console.log("xpos/50");
+                    this.leftExp = 1;
+                    j = player.range + 1;
+                    i = boxes.length + 1;
+
+                    //om den går igenom hela loopen plyer leftexp = player.range
+                } else if (i == boxes.length - 1) {
+                    console.log("sista else this up = player range vänster");
                     this.leftExp = player.range;
                 }
             }
         }
+        //down spräng
         for (var j = 0; j <= player.range; j++) {
 
             for (var i = 0; i < boxes.length; i++) {
 
                 if (this.yPos + j * 50 == boxes[i].yPos && this.xPos == boxes[i].xPos) {
 
-                    console.log("kolla ner");
+
+                    console.log(j);
+
                     //ge chans till att släppa boost
                     if (Math.random() > 0.7) {
+
                         console.log("boost skapad");
                         boosters.push(new Boost(boxes[i].xPos, boxes[i].yPos));
                     }
-                    boxes.splice(i, 1);
-                    this.downExp = j;
 
+                    //ta bort box och stäng av loop samt ge downExp nytt värde för hur långt den ska spränga
+                    boxes.splice(i, 1);
+                    console.log("kolla upp");
+                    this.downExp = j;
+                    j = player.range + 1;
                     i = boxes.length + 1;
-                } else {
-                    
+
+                    //stäng av loop om den exploderar i sten
+                } else if ((this.xPos / 50) % 2 !== 0) {
+                    console.log("xpos/50");
+                    this.downExp = 1;
+                    j = player.range + 1;
+                    i = boxes.length + 1;
+
+                    //om den går igenom hela loopen plyer downExp = player.range
+                } else if (i == boxes.length - 1) {
+                    //console.log("sista else this up = player range");
                     this.downExp = player.range;
                 }
             }
         }
+        //up spräng
         for (var j = 0; j <= player.range; j++) {
 
             for (var i = 0; i < boxes.length; i++) {
 
                 if (this.yPos - j * 50 == boxes[i].yPos && this.xPos == boxes[i].xPos) {
-                    
-                    
-                    
+
+
                     console.log(j);
 
-                    
                     //ge chans till att släppa boost
                     if (Math.random() > 0.7) {
-                        
+
                         console.log("boost skapad");
                         boosters.push(new Boost(boxes[i].xPos, boxes[i].yPos));
                     }
-                    
+
+                    //ta bort box och stäng av loop samt ge upExp nytt värde för hur långt den ska spränga
                     boxes.splice(i, 1);
                     console.log("kolla upp");
                     this.upExp = j;
                     j = player.range + 1;
-                    
+                    i = boxes.length + 1;
+
+                    //stäng av loop om den exploderar i sten
                 } else if ((this.xPos / 50) % 2 !== 0) {
                     console.log("xpos/50");
                     this.upExp = 1;
-                    
-                } else {
-                    console.log("sista else this up = player range");
+                    j = player.range + 1;
+                    i = boxes.length + 1;
+
+                    //om den går igenom hela loopen plyer upExp = player.range
+                } else if (i == boxes.length - 1) {
+                    //console.log("sista else this up = player range");
                     this.upExp = player.range;
                 }
             }
@@ -190,11 +236,11 @@ function Explosion(startX, startY, whichPlayer, teamColour, explosionRange) {
         //kolla om någon player är i explosionen
         for (var i = 1; i < players.length; i++) {
 
-            if (this.xPos - (player.range * 50) <= players[i].xPos &&
-                players[i].xPos <= this.xPos + (player.range * 50) &&
+            if (this.xPos - (this.leftExp * 50) <= players[i].xPos &&
+                players[i].xPos <= this.xPos + (this.rightExp * 50) &&
                 this.yPos == players[i].yPos ||
-                this.yPos - (player.range * 50) <= players[i].yPos &&
-                players[i].yPos <= this.yPos + (player.range * 50) &&
+                this.yPos - (this.upExp * 50) <= players[i].yPos &&
+                players[i].yPos <= this.yPos + (this.downExp * 50) &&
                 this.xPos == players[i].xPos) {
 
                 console.log(players[i].yPos, this.yPos + player.range * 50, players[i].xPos, this.xPos + player.range * 50);
